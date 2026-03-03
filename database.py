feea43281b82812c6080aa3b2fd1dcfa89f5fc91
Mathgg722 <familiaaraujograciano@gmail.com>
2026-03-03 16:57:29 -0300

    debug worker and api database

--- database.py
+++ database.py
@@ -2,26 +2,26 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-# Pega a variável de ambiente do Railway
+# Pega a variável de ambiente
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 if not DATABASE_URL:
-    raise Exception("DATABASE_URL não encontrada nas variáveis de ambiente")
+    raise Exception("❌ DATABASE_URL não encontrada nas variáveis de ambiente")
 
 print("🔎 CONECTANDO NO BANCO:", DATABASE_URL)
 
-# Cria engine do SQLAlchemy
+# Engine do SQLAlchemy
 engine = create_engine(
     DATABASE_URL,
     pool_pre_ping=True
 )
 
-# Cria sessão
+# Sessão
 SessionLocal = sessionmaker(
     autocommit=False,
     autoflush=False,
     bind=engine
 )
 
-# Base dos modelos
+# Base para os models
 Base = declarative_base()
