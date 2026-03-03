bce9a0591209d2c579cd42cfb0891ad6e96ad655
Mathgg722 <familiaaraujograciano@gmail.com>
2026-03-03 16:18:57 -0300

    proper db dependency and markets endpoint

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from sqlalchemy.orm import Session
 from database import SessionLocal, engine
-from models import Base, Market, Token, Snapshot
+from models import Base, Market
+from typing import List
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
 
+# Dependency correta
 def get_db():
     db = SessionLocal()
     try:
@@ -22,23 +24,25 @@ def home():
 
 
 @app.get("/status")
-def status():
-    db: Session = next(get_db())
+def status(db: Session = Depends(get_db)):
     count = db.query(Market).count()
     return {"stored_markets": count}
 
 
 @app.get("/markets")
-def markets():
-    db: Session = next(get_db())
+def get_markets(db: Session = Depends(get_db)):
     markets = db.query(Market).all()
 
     result = []
+
     for m in markets:
         result.append({
+            "id": m.id,
             "question": m.question,
             "slug": m.market_slug,
             "end_date": m.end_date,
+            "volume": m.volume,
+            "liquidity": m.liquidity,
             "tokens": [
                 {
                     "outcome": t.outcome,
