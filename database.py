f69233309e314fba6ff10cea3b54e8638e03e7eb
Mathgg722 <familiaaraujograciano@gmail.com>
2026-03-12 15:42:01 -0300

    fix Base no database.py

--- database.py
+++ database.py
@@ -1,6 +1,8 @@
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.ext.declarative import declarative_base
+Base = declarative_base()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
